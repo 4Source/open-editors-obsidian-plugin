b1546c890dcb65431dc4bd0af10fb0c58f77ac4a
4Source <38220764+4Source@users.noreply.github.com>
2024-12-15 00:21:48 +0100

    Better support for popout windows

--- src/OpenEditorsView.ts
+++ src/OpenEditorsView.ts
@@ -68,36 +68,118 @@ export class OpenEditorsView extends View {
 
 			// Recursively build the tree structure for all child elements
 			main.children.forEach((element: { id: string, type: string, children: object[], state: { title: string, type: string } }) => {
-				TreeWalker(element, tree);
+				this.TreeWalker(element, tree);
 			});
 		}
 
 		// Handle floating windows (popout windows)
 		const floatingWindow = layout['floating'];
 		if (floatingWindow) {
-			// Counter to label floating windows
-			let count = 1;
-			floatingWindow.children.forEach((element: { id: string, type: string, children: object[] }) => {
+			floatingWindow.children.forEach((element: { id: string, type: string, children: object[], state: { title: string, type: string } }) => {
+				this.TreeWalker(element, undefined);
+			});
+		}
+	}
+
+	/**
+	 * A recursive function to traverse the workspace layout and build the tree structure.
+	 * @param layout - The current layout node being processed.
+	 * @param parent - The parent TreeItem to attach new child TreeItems to.
+	 */
+	TreeWalker (layout: { id: string, type: string, children: object[], state: { title: string, type: string } }, parent: TreeItem | undefined) {
+		// Counter for labeling groups and tabs
+		let count = 1;
+		switch (layout.type) {
+			// Handle popout window layouts
+			case 'window':
 				// Create a tree item for each floating window
-				const tree = new TreeItem(this.treeEl, `Window ${count}`, element.id, undefined, [{
+				parent = new TreeItem(this.treeEl, `Window ${count}`, layout.id, undefined, [{
 					iconId: ICON_CLOSE_WINDOW,
 					onClickCallback: () => {
 						// Recursively detach all leaves in the tree
-						tree.rekursiveCall((tree) => {
-							this.app.workspace.getLeafById(tree.id)?.detach();
+						parent?.rekursiveCall((parent) => {
+							this.app.workspace.getLeafById(parent.id)?.detach();
 						});
 					},
 					ariaLabel: 'Close all',
 				}]);
 				// Add the tree item to the list of windows
-				this.windows.push(tree);
+				this.windows.push(parent);
 
-				// Recursively build the tree structure for all child elements
-				element.children.forEach((element_: { id: string, type: string, children: object[], state: { title: string, type: string } }) => {
-					TreeWalker(element_, tree);
+				if (layout.children.length > 1) {
+					let groupCount = 1;
+					// Recursively build the tree structure for all child elements
+					layout.children.forEach((element: { id: string, type: string, children: object[], state: { title: string, type: string } }) => {
+						const group = parent?.addChild((containerEl) => new TreeItem(containerEl, `Group ${groupCount}`, element.id, undefined, [{
+							iconId: ICON_CLOSE_GROUP,
+							onClickCallback: () => {
+								parent?.rekursiveCall((tree) => {
+									this.app.workspace.getLeafById(tree.id)?.detach();
+								});
+							},
+							ariaLabel: 'Close all',
+						}]));
+						this.TreeWalker(element, group);
+						groupCount++;
+					});
+				}
+				else {
+					// Recursively build the tree structure for all child elements
+					layout.children.forEach((element: { id: string, type: string, children: object[], state: { title: string, type: string } }) => {
+						this.TreeWalker(element, parent);
+					});
+					count++;
+				}
+
+				break;
+			// Handle split layouts
+			case 'split':
+				layout.children.forEach((element: { id: string, type: string, children: object[], state: { title: string, type: string } }) => {
+					// Create a child tree item for each split group
+					const tree = parent?.addChild((container) => new TreeItem(container, `Group ${count}`, element.id, undefined, [{
+						iconId: ICON_CLOSE_GROUP,
+						onClickCallback: () => {
+							tree?.rekursiveCall((tree) => {
+								this.app.workspace.getLeafById(tree.id)?.detach();
+							});
+						},
+						ariaLabel: 'Close all',
+					}]));
+					// Recursively process tabs as children
+					this.TreeWalker(element, tree);
+					count++;
 				});
-				count++;
-			});
+				break;
+			// Handle tabs layouts
+			case 'tabs':
+				layout.children.forEach((element: { id: string, type: string, children: object[], state: { title: string, type: string } }) => {
+					this.TreeWalker(element, parent);
+				});
+				break;
+			// Handle individual leaves
+			case 'leaf':
+				parent?.addChild((container) => new TreeItem(container, layout.state.title, layout.id, {
+					onClickCallback: () => {
+						// Set the clicked leaf as the active leaf in the workspace
+						const leaf = this.app.workspace.getLeafById(layout.id);
+						if (!leaf) {
+							console.warn('Leaf not found with the id ', layout.id);
+							return;
+						}
+						this.app.workspace.setActiveLeaf(leaf);
+						// TODO: Bring the leaf's popout window to the foreground if necessary
+					},
+				}, [{
+					iconId: ICON_CLOSE,
+					onClickCallback: () => {
+						this.app.workspace.getLeafById(layout.id)?.detach();
+					},
+					ariaLabel: 'Close',
+				}]));
+				break;
+			default:
+				console.warn('Unknown layout component', layout);
+				break;
 		}
 	}
 
@@ -112,66 +194,5 @@ export class OpenEditorsView extends View {
 
 		// Clear the tree container in the DOM
 		this.treeEl.empty();
-	}
-}
-
-/**
- * A recursive function to traverse the workspace layout and build the tree structure.
- * @param layout - The current layout node being processed.
- * @param parent - The parent TreeItem to attach new child TreeItems to.
- */
-function TreeWalker (layout: { id: string, type: string, children: object[], state: { title: string, type: string } }, parent: TreeItem) {
-	// Counter for labeling groups and tabs
-	let count = 1;
-	switch (layout.type) {
-		// Handle split layouts (groups of tabs)
-		case 'split':
-			layout.children.forEach((element: { id: string, type: string, children: object[], state: { title: string, type: string } }) => {
-			// Create a child tree item for each split group
-				const tree = parent.addChild((container) => new TreeItem(container, `Group ${count}`, element.id, undefined, [{
-					iconId: ICON_CLOSE_GROUP,
-					onClickCallback: () => {
-						tree.rekursiveCall((tree) => {
-							this.app.workspace.getLeafById(tree.id)?.detach();
-						});
-					},
-					ariaLabel: 'Close all',
-				}]));
-				// Recursively process child elements
-				TreeWalker(element, tree);
-				count++;
-			});
-			break;
-		// Handle tabs layouts
-		case 'tabs':
-			layout.children.forEach((element: { id: string, type: string, children: object[], state: { title: string, type: string } }) => {
-			// Recursively process tabs as children
-				TreeWalker(element, parent);
-			});
-			break;
-		// Handle individual leaves
-		case 'leaf':
-			parent.addChild((container) => new TreeItem(container, layout.state.title, layout.id, {
-				onClickCallback: () => {
-					// Set the clicked leaf as the active leaf in the workspace
-					const leaf = this.app.workspace.getLeafById(layout.id);
-					if (!leaf) {
-						console.warn('Leaf not found with the id ', layout.id);
-						return;
-					}
-					this.app.workspace.setActiveLeaf(leaf);
-					// TODO: Bring the leaf's popout window to the foreground if necessary
-				},
-			}, [{
-				iconId: ICON_CLOSE,
-				onClickCallback: () => {
-					this.app.workspace.getLeafById(layout.id)?.detach();
-				},
-				ariaLabel: 'Close',
-			}]));
-			break;
-		default:
-			console.warn('Unknown layout component', layout);
-			break;
 	}
 }
